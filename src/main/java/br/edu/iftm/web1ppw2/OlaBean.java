209971fd5837a88241737e69a9c63cbead504fcd
André Victor <apolofostry@gmail.com>
2023-09-06 20:33:40 -0300

    Primefaces added

---
+++ src/main/java/br/edu/iftm/web1ppw2/OlaBean.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.iftm.web1ppw2;
+
+/**
+ *
+ * @author IFTM
+ */
+public class OlaBean {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
